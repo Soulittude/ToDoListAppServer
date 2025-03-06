6e0fa5e8fe04726456a4c3cdb33e85bd4bb119d6
Soulittude <greatmaotun@gmail.com>
2025-03-06 23:52:45 +0300

    Core App

--- src/routes/todoRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getTodos } from '../controllers/todoController';
-
-const router = Router();
-
-router.get('/', getTodos);
-// Add POST, PUT, DELETE routes
-
-export default router;
